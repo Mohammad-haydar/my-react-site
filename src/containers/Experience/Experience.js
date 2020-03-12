439269bd5b5039dd0590da127789cb6da576d185
Melissa Astbury <melissajastbury@gmail.com>
2020-03-12 20:15:24 +0000

    adding a bottom border into my header when active

--- src/containers/Experience/Experience.js
+++ src/containers/Experience/Experience.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './Experience.scss';
+import "./Experience.scss";
 
 const Experience = () => {
-    return (
-        <h1>Experience</h1>
-    )
+  return (
+    <section>
+      <div className="wrapper">
+        <div className="experience-container">
+          <h1>Experience</h1>
+          <p>
+            Generates and inserts lorem ipsum textGenerates and inserts lorem
+            ipsum textGenerates and inserts lorem ipsum textGenerates and
+            inserts lorem ipsum textGenerates and inserts lorem ipsum text
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
