fc771d5c54ce5bdd97275fdec0b58b02f87438f4
Melissa Astbury <melissajastbury@gmail.com>
2020-03-20 21:23:10 +0000

    adding in dates, amended css for mobile

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,112 +2,112 @@ import React from "react";
 
 import "./Cards.scss";
 
-const Cards = () => {
-  return (
-    <div className="cards-container">
-      <div className="card">
-        <h3>Case Owner</h3>
-        <p className="job-title">TFC Homeloans - Finance</p>
-        <p>• Support all members of the team with their diaries</p>
-        <p>• Deal with a high volume of inbound and outbound calls/emails</p>
-        <p>• Deal with various enquires from lenders and customers</p>
-        <p>• Enter information into the system and excel spreadsheets</p>
-        <p>• Dispose of confidential information</p>
-        <p>
-          • Review applicants documents carefully to produce a fully packaged
-          case to the lenders
-        </p>
-        <p>
-          • Attend training programs to learn more information about the
-          products and service we sell
-        </p>
-        <p>
-          • Keep all files up to date on the system to keep brokers informed
-        </p>
-        <p>• Take application fees securly</p>
-      </div>
-      <div className="card">
-        <h3>Team Operations</h3>
-        <p className="job-title">Monarch Holidays - Travel</p>
-        <p>• A split role of an operations and customer care</p>
-        <p>
-          • Dealt with a high volume of phone calls and emails every day which
-          were monitored
-        </p>
-        <p>
-          • Created spreadsheets to improve the efficiency of how the business
-          works and communicates
-        </p>
-        <p>
-          • Dealt with clients who need welfare support when travelling abroad
-        </p>
-        <p>
-          • Contacted hotels to find out more information regarding the
-          customers’ welfare
-        </p>
-        <p>
-          • Used quick judgement when there was a constant high demand of work
-        </p>
-        <p>• Worked as a team to provide support to all staff in busy areas</p>
-        <p>• Kept a track of files by week order</p>
-        <p>• Logged welfare requests in an excel spreadsheet</p>
-        <p>
-          • Followed up with other staff members to work in times of a staff
-          shortage
-        </p>
-      </div>
-      <div className="card">
-        <h3>Administration</h3>
-        <p className="job-title">Connell Sixth Form - Education</p>
-        <p>• Level 2 Business Administration and Level 2 ICT.</p>
-        <p>
-          • Attended various marketing events to attract new high school leavers
-          into being a Connell student
-        </p>
-        <p>
-          • Communicated with both parents and students dealing with any issues
-          or enquires
-        </p>
-        <p>
-          • Managed Reprographics which needs to be kept organised to meet the
-          high demand from teachers and to be more efficient to keep track of
-          the stock
-        </p>
-        <p>
-          • Learnt policies and regulations which the Education System provides
-          all across the schools
-        </p>
-        <p>
-          • Spent time on reception, providing customer service to the high
-          standards expected
-        </p>
-        <p>
-          • Delivered the post every day at the correct time so that important
-          mail can be distributed
-        </p>
-        <p>
-          • Uploaded and checked over the admissions folders so that new and old
-          applicants were kept up to date in their files in organised cabinets
-        </p>
-        <p>• Used excel spreadsheets to input student data</p>
-        <p>
-          • Used Photoshop and Illustrator to generate high-quality marketing
-          leaflets
-        </p>
-        <p>
-          • Set up and organised Open Day events so that we have the right
-          equipment required on the day.
-        </p>
-        <p>
-          • Achieved the ‘Apprentice Star’ award which has motivated me to do
-          more and take on more challenging tasks.
-        </p>
-        <p>• Supported the ‘Student Recruitment’ manager</p>
-        <p>• Added applications onto SIMs</p>
-        <p>• Provided support for students</p>
-      </div>
+const Cards = () => (
+  <div className="cards-container">
+    <div className="card">
+      <h3>Case Owner</h3>
+      {/* <span className="job-dates">October 2017 - Present</span> */}
+      <p className="job-dates">October 2017 - Present</p>
+      <p className="job-title">TFC Homeloans - Finance</p>
+      <p>• Support all members of the team with their diaries</p>
+      <p>• Deal with a high volume of inbound and outbound calls/emails</p>
+      <p>• Deal with various enquires from lenders and customers</p>
+      <p>• Enter information into the system and excel spreadsheets</p>
+      <p>• Dispose of confidential information</p>
+      <p>
+        • Review applicants documents carefully to produce a fully packaged case
+        to the lenders
+      </p>
+      <p>
+        • Attend training programs to learn more information about the products
+        and service we sell
+      </p>
+      <p>• Keep all files up to date on the system to keep brokers informed</p>
+      <p>• Take application fees securly</p>
     </div>
-  );
-};
+    <div className="card">
+      <h3>Team Operations</h3>
+      <p className="job-dates">April 2017 - September 2017</p>
+      <p className="job-title">Monarch Holidays - Travel</p>
+      <p>• A split role of an operations and customer care</p>
+      <p>
+        • Dealt with a high volume of phone calls and emails every day which
+        were monitored
+      </p>
+      <p>
+        • Created spreadsheets to improve the efficiency of how the business
+        works and communicates
+      </p>
+      <p>
+        • Dealt with clients who need welfare support when travelling abroad
+      </p>
+      <p>
+        • Contacted hotels to find out more information regarding the customers’
+        welfare
+      </p>
+      <p>
+        • Used quick judgement when there was a constant high demand of work
+      </p>
+      <p>• Worked as a team to provide support to all staff in busy areas</p>
+      <p>• Kept a track of files by week order</p>
+      <p>• Logged welfare requests in an excel spreadsheet</p>
+      <p>
+        • Followed up with other staff members to work in times of a staff
+        shortage
+      </p>
+    </div>
+    <div className="card">
+      <h3>Administration</h3>
+      <p className="job-dates">August 2016 - April 2017</p>
+      <p className="job-title">Connell Sixth Form - Education</p>
+      <p>• Level 2 Business Administration and Level 2 ICT.</p>
+      <p>
+        • Attended various marketing events to attract new high school leavers
+        into being a Connell student
+      </p>
+      <p>
+        • Communicated with both parents and students dealing with any issues or
+        enquires
+      </p>
+      <p>
+        • Managed Reprographics which needs to be kept organised to meet the
+        high demand from teachers and to be more efficient to keep track of the
+        stock
+      </p>
+      <p>
+        • Learnt policies and regulations which the Education System provides
+        all across the schools
+      </p>
+      <p>
+        • Spent time on reception, providing customer service to the high
+        standards expected
+      </p>
+      <p>
+        • Delivered the post every day at the correct time so that important
+        mail can be distributed
+      </p>
+      <p>
+        • Uploaded and checked over the admissions folders so that new and old
+        applicants were kept up to date in their files in organised cabinets
+      </p>
+      <p>• Used excel spreadsheets to input student data</p>
+      <p>
+        • Used Photoshop and Illustrator to generate high-quality marketing
+        leaflets
+      </p>
+      <p>
+        • Set up and organised Open Day events so that we have the right
+        equipment required on the day.
+      </p>
+      <p>
+        • Achieved the ‘Apprentice Star’ award which has motivated me to do more
+        and take on more challenging tasks.
+      </p>
+      <p>• Supported the ‘Student Recruitment’ manager</p>
+      <p>• Added applications onto SIMs</p>
+      <p>• Provided support for students</p>
+    </div>
+  </div>
+);
 
 export default Cards;
